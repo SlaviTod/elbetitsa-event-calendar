0550638fc51a4efa34a0b9fa15c509f1746a4c2c
SlaviTod <slavka_todorova@abv.bg>
2026-03-02 20:26:31 +0200

    try fix build error

--- calendar/components/themed/themed-radio-button-group.tsx
+++ calendar/components/themed/themed-radio-button-group.tsx
@@ -1,43 +1,43 @@
-import { StyleProp, ViewStyle } from "react-native";
-import { Host, Column, RadioButton, ListItem } from '@expo/ui/jetpack-compose';
-import { ThemedView } from "./themed-view";
-import { useTranslation } from "react-i18next";
+// import { StyleProp, ViewStyle } from "react-native";
+// import { Host, Column, RadioButton, ListItem } from '@expo/ui/jetpack-compose';
+// import { ThemedView } from "./themed-view";
+// import { useTranslation } from "react-i18next";
 
 
-type TRBProps = {
-  options: string[],
-  selectedOption: string;
-  styleWr?: StyleProp<ViewStyle>,
-  onSelect: (option: string) => void,
-}
+// type TRBProps = {
+//   options: string[],
+//   selectedOption: string;
+//   styleWr?: StyleProp<ViewStyle>,
+//   onSelect: (option: string) => void,
+// }
 
-export const ThemedRadioButtonGroup = ({
-  options,
-  selectedOption,
-  styleWr,
-  onSelect,
-}: TRBProps) => {
+// export const ThemedRadioButtonGroup = ({
+//   options,
+//   selectedOption,
+//   styleWr,
+//   onSelect,
+// }: TRBProps) => {
 
-  const { t } = useTranslation();
+//   const { t } = useTranslation();
 
-  return (<>
-    <ThemedView style={styleWr}>
-      <Host matchContents>
-        <Column>
-        {!!options.length && options.map((op) => <>
-        {/* @ts-expect-error */}
-          <ListItem key={op} headline={t(op)} onPress={() => onSelect(el)}>
-            <ListItem.Trailing>
-              <RadioButton
-                selected={selectedOption === op}
-                onClick={() => onSelect(op)}
-              />
-            </ListItem.Trailing>
-          </ListItem>
-        </>)
-        }
-        </Column>
-      </Host>
-    </ThemedView>
-  </>);
-}
+//   return (<>
+//     <ThemedView style={styleWr}>
+//       <Host matchContents>
+//         <Column>
+//         {!!options.length && options.map((op) => <>
+//         {/* @ts-expect-error */}
+//           <ListItem key={op} headline={t(op)} onPress={() => onSelect(el)}>
+//             <ListItem.Trailing>
+//               <RadioButton
+//                 selected={selectedOption === op}
+//                 onClick={() => onSelect(op)}
+//               />
+//             </ListItem.Trailing>
+//           </ListItem>
+//         </>)
+//         }
+//         </Column>
+//       </Host>
+//     </ThemedView>
+//   </>);
+// }
